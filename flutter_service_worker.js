95751c7fea1de2284ab18262ed4d221f59b6108c
ko-htut <ko-htut@users.noreply.github.com>
2022-10-19 03:25:14 +0000

    deploy: 6958bea1b33b6e5a6d37a26fac25e7b4ae872d8a

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"main.dart.js": "f113af3dc821b04f3e86e35bb8b6fba7",
-"index.html": "b49e2d77b92bba0c584417325b2280dc",
-"/": "b49e2d77b92bba0c584417325b2280dc",
+"main.dart.js": "fc1b76739ce557bb800cf8714d5938d5",
+"index.html": "18a94ba908667a78d5b48c2621977aa4",
+"/": "18a94ba908667a78d5b48c2621977aa4",
 "flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
 "manifest.json": "ee26cc44c12415df9ef7206db819f549",
 "canvaskit/canvaskit.js": "2bc454a691c631b07a9307ac4ca47797",
@@ -18,7 +18,7 @@ const RESOURCES = {
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/fonts/MaterialIcons-Regular.otf": "95db9098c58fd6db106f1116bae85a0b",
-"assets/NOTICES": "ee860d72fa5410a27f2bd680ba94aaba",
+"assets/NOTICES": "1cba9986ff5668d5afe104f34880ecab",
 "version.json": "29d6761912d513f0702995292ef5cee5",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
