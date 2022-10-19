6958bea1b33b6e5a6d37a26fac25e7b4ae872d8a
Ko Htut <minthanhtut@abank.com.mm>
2022-10-19 09:53:06 +0630

    fx

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 
 class AppWidget extends StatelessWidget {
   const AppWidget({Key? key}) : super(key: key);
@@ -11,6 +12,16 @@ class AppWidget extends StatelessWidget {
       theme: ThemeData(useMaterial3: true),
       routeInformationParser: Modular.routeInformationParser,
       routerDelegate: Modular.routerDelegate,
+      builder: (context, child) => ResponsiveWrapper.builder(child,
+          maxWidth: 1200,
+          minWidth: 480,
+          defaultScale: true,
+          breakpoints: const [
+            ResponsiveBreakpoint.resize(480, name: MOBILE),
+            ResponsiveBreakpoint.autoScale(800, name: TABLET),
+            ResponsiveBreakpoint.resize(1000, name: DESKTOP),
+          ],
+          background: Container(color: const Color(0xFFF5F5F5))),
     ); //added by extension
   }
 }
--- lib/module/home/widget/home_widget.dart
+++ lib/module/home/widget/home_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:json_tree_viewer/json_tree_viewer.dart';
 
 class HomeWidget extends StatefulWidget {
   const HomeWidget({Key? key}) : super(key: key);
@@ -14,8 +15,116 @@ class _HomeWidgetState extends State<HomeWidget> {
       appBar: AppBar(
         title: const Text("Home"),
       ),
-      body: const Center(
-        child: Text("Hello My friend"),
+      body: SingleChildScrollView(
+        physics: const NeverScrollableScrollPhysics(),
+        child: Column(
+          children: [
+            _buttom(name: "Biller Category"),
+            // const Text("Sent Qureery"),
+            //  JsonTreeViewer(
+            //   expandedAll: true,
+            //   data: {
+            //     "ErrorCode": "00",
+            //     "ErrorMessage": "Success",
+            //     "CategoryList": [
+            //       {
+            //         "CategoryCode": "topup",
+            //         "CategoryName": "Top Up ( don't delete plz )"
+            //       }
+            //     ]
+            //   },
+            // ),
+            // const Text("Respnse "),
+            // const JsonTreeViewer(
+            //   data: {
+            //     "ErrorCode": "00",
+            //     "ErrorMessage": "Success",
+            //     "CategoryList": [
+            //       {
+            //         "CategoryCode": "topup",
+            //         "CategoryName": "Top Up ( don't delete plz )"
+            //       }
+            //     ]
+            //   },
+            // ),
+            // _buttom(name: "Biller Product"),
+            // const Text("Sent Qureery"),
+            // const JsonTreeViewer(
+            //     data: {"channelCode": "AbankMM", "billerCode": "OoredooEload"}),
+            // const Text("Respnse "),
+            // const JsonTreeViewer(data: {
+            //   "ErrorCode": "00",
+            //   "ErrorMessage": "Success",
+            //   "BillerCategory": null,
+            //   "BillerCode": "OoredooEload",
+            //   "BillerName": null,
+            //   "BillerLogo": null,
+            //   "BillerCountry": null,
+            //   "BillerCurrency": null,
+            //   "ProductList": [
+            //     {
+            //       "ProductCode": "500",
+            //       "ProductName": "500",
+            //       "ProductAmount": "500"
+            //     },
+                
+            //   ]
+            // }),
+            // _buttom(name: "Enquiry"),
+            // const Text("Sent Qureery"),
+            // const JsonTreeViewer(data: {
+            //   "ChannelCode": "AbankMM",
+            //   "BillerCode": "OoredooEload",
+            //   "Detail": "{\"Deno\":\"1000\",\"MobileNumber\":\"09964233241\"}"
+            // }),
+            // const Text("Respnse "),
+            // const JsonTreeViewer(data: {
+            //   "ErrorCode": "00",
+            //   "ErrorMessage": "Success.",
+            //   "ChannelAmount": 1150.00,
+            //   "TransactionAmount": 1000,
+            //   "Detail": "{\"Deno\":\"1000\",\"MobileNumber\":\"09964233241\"}"
+            // }),
+            // _buttom(name: "Confirm"),
+            // const Text("Sent Qureery"),
+            // const JsonTreeViewer(
+            //   data: {
+            //     "ChannelCode": "AbankMM",
+            //     "BillerCode": "OoredooEload",
+            //     "ChannelRefId": "KH090003",
+            //     "TransactionAmount": 3000,
+            //     "Detail": "{\"Deno\":\"3000\",\"MobileNumber\":\"09964233241\"}"
+            //   },
+            // ),
+            // const Text("Respnse "),
+            // const JsonTreeViewer(data: {
+            //   "ErrorCode": "00",
+            //   "ErrorMessage": "Success",
+            //   "Detail": "{\"Deno\":\"3000\",\"MobileNumber\":\"09964233241\"}",
+            //   "ChannelRefId": "KH090003",
+            //   "BillerRefId": null,
+            //   "BpaTxnId": "40DA07EA79",
+            //   "ChannelAmount": 3250.00,
+            //   "TransactionAmount": 3000
+            // }),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buttom({required String name}) {
+    return Container(
+      height: 65,
+      width: double.infinity,
+      margin: const EdgeInsets.all(8),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(5), color: Colors.indigoAccent),
+      child: Center(
+        child: Text(
+          name,
+          style: const TextStyle(color: Colors.white),
+        ),
       ),
     );
   }
