683790a248147270c75203ec8899782253919a84
ko-htut <ko-htut@users.noreply.github.com>
2022-10-20 04:50:01 +0000

    deploy: e517a05fb59507b653e5eb31a99bde837eb42053

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"main.dart.js": "a5ca80a423dfa019bbae2392fc7d7b8a",
-"index.html": "6be7b7e88252807a3f59ee3165a38cc4",
-"/": "6be7b7e88252807a3f59ee3165a38cc4",
+"main.dart.js": "5241b26e14deb78b097a3adbfb448dcd",
+"index.html": "d0ba27624b90199250aba1c21e35e834",
+"/": "d0ba27624b90199250aba1c21e35e834",
 "flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
 "manifest.json": "ee26cc44c12415df9ef7206db819f549",
 "canvaskit/canvaskit.js": "2bc454a691c631b07a9307ac4ca47797",
@@ -14,7 +14,7 @@ const RESOURCES = {
 "canvaskit/profiling/canvaskit.js": "38164e5a72bdad0faa4ce740c9b8e564",
 "canvaskit/profiling/canvaskit.wasm": "95a45378b69e77af5ed2bc72b2209b94",
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
-"assets/shaders/ink_sparkle.frag": "237ceacd6c04871e538aaa934b1327a3",
+"assets/shaders/ink_sparkle.frag": "bb6af69544daba10b8bb89e60edaa055",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/fonts/MaterialIcons-Regular.otf": "95db9098c58fd6db106f1116bae85a0b",
